6c06c5836c764da0ce5d6eaba297e4f1a5dfe7f7
maciej simka <mcj.simka@gmail.com>
2021-01-30 21:28:29 +0100

    Adjust configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
+let tailwindConfig = process.env.HUGO_FILE_TAILWIND_CONFIG_JS || './tailwind.config.js';
+const tailwind = require('tailwindcss')(tailwindConfig);
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
-}
+	plugins: [ tailwind, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [ autoprefixer ] : []) ]
+};
