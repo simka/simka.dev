9a65fc88aa057f118f88bb067a26d085002e3642
maciej simka <mcj.simka@gmail.com>
2021-05-15 17:12:09 +0200

    Temporarily ignore TS error

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -2,6 +2,7 @@ import remark from "remark";
 import html from "remark-html";
 
 export async function markdownToHtml(markdown: string): Promise<string> {
+// @ts-ignore
   const result = await remark().use(html).process(markdown);
   return result.toString();
 }
