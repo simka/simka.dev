4daa148ee056eb4dc00133fcdeb36d7042d36eaf
maciej simka <mcj.simka@gmail.com>
2021-09-15 17:05:08 +0200

    Replace index page image and preload it

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 
-import homeImage from "../assets/index.webp";
+import homeImage from "../assets/index.jpg";
 import Layout from "../components/Layout";
 import Text from "../components/Text";
 import Link from "../components/Link";
@@ -19,7 +19,7 @@ function Home() {
             <Link href="https://github.com/simka">software</Link>, photos and{" "}
             <Link href="https://soundcloud.com/day_sleeper">sounds</Link>.
           </Text>
-          <Image src={homeImage} placeholder="blur" />
+          <Image src={homeImage} placeholder="blur" priority />
         </article>
       </Layout>
     </>
