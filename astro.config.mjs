5932672b5e27bb66b6e079e11695a775096512ab
maciej simka <mcj.simka@gmail.com>
2023-01-28 13:12:30 +0100

    start from scratch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
+import mdx from "@astrojs/mdx";
 import image from "@astrojs/image";
+import prefetch from "@astrojs/prefetch";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  integrations: [
+    react(),
+    tailwind(),
+    mdx(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    prefetch(),
+    partytown(),
+  ],
 });
