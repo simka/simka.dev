43e5623cd9d0350f1b6571bebc6453607ad54dab
maciej simka <mcj.simka@gmail.com>
2023-12-10 16:57:56 +0100

    improve tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./content/**/*.{md,njk,html}"],
+  content: ["./content/**/*.{md,njk,html}", "./_includes/**/*.{md,njk,html}"],
   theme: {
     extend: {},
   },
