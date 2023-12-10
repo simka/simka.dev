ba62f0f05e74574ba2d3f5b1b9534a3298d60e8c
maciej simka <mcj.simka@gmail.com>
2023-12-10 14:19:43 +0100

    init 11ty

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: "maciej simka",
+  url: "https://simka.dev",
+  language: "en",
+  description: "personal website of maciej simka",
+  author: {
+    name: "maciej simka",
+    email: "mcj.simka@gmail.com",
+    url: "https://simka.dev",
+  },
+};
